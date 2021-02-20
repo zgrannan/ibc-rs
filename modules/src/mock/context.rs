d1dccf48817d19e08a693108df760f111f7309ed
Vitor Enes <vitorenesduarte@gmail.com>
2021-02-20 13:45:14 +0100

    minimize diff

--- modules/src/mock/context.rs
+++ modules/src/mock/context.rs
@@ -88,11 +88,11 @@ pub struct MockContext {
     /// All the channels in the store. TODO Make new key PortId X ChanneId
     channels: BTreeMap<(PortId, ChannelId), ChannelEnd>,
 
-    /// Association between conection ids and channel ids.
-    connection_channels: BTreeMap<ConnectionId, Vec<(PortId, ChannelId)>>,
-
     /// Counter for channel identifiers (see `increase_channel_counter`).
     channel_ids_counter: u64,
+
+    /// Association between conection ids and channel ids.
+    connection_channels: BTreeMap<ConnectionId, Vec<(PortId, ChannelId)>>,
 
     /// Tracks the sequence number for the next packet to be sent.
     next_sequence_send: BTreeMap<(PortId, ChannelId), u64>,
