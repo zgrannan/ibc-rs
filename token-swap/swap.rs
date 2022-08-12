f2fff093c78588f84a0fae7f09132698cf8a2f63
Zack Grannan <zgrannan@protonmail.com>
2022-08-11 22:19:49 -0700

    Bank spec

---
+++ token-swap/swap.rs
@@ -0,0 +1,92 @@
+use std::collections::HashSet;
+use std::collections::HashMap;
+use prusti_contracts::*;
+type CoinId = u32;
+type AccountId = u32;
+type Ics20Error = u32;
+
+#[derive(Clone, Copy)]
+struct PrefixedCoin {
+    denom: CoinId,
+    amount: u32
+}
+
+struct Bank {
+    accounts: HashMap<AccountId, HashSet<PrefixedCoin>>
+}
+
+#[extern_spec]
+impl<T: PartialEq, U: std::fmt::Debug + PartialEq> std::result::Result<T, U> {
+    #[pure]
+    #[ensures(matches!(&self, Ok(_)) == result)]
+    pub fn is_ok(&self) -> bool;
+
+    #[pure]
+    #[requires(self.is_ok())]
+    #[ensures(self == Ok(result))]
+    pub fn unwrap(self) -> T;
+}
+
+impl Bank {
+
+    #[pure]
+    #[trusted]
+    fn balance(&self, accountId: AccountId, coin_id: CoinId) -> u32 {
+        unimplemented!()
+    }
+
+    #[requires(self.balance(acct1, amt.denom) >= amt.amount)]
+    #[ensures(
+        forall(|acct_id: AccountId, coin_id: CoinId| self.balance(acct_id, coin_id) == old(self).balance(acct_id, coin_id))
+    )]
+    fn send_coins_involution(&mut self, acct1: AccountId, acct2: AccountId, amt: PrefixedCoin) {
+        self.send_coins(acct1, acct2, amt);
+        self.send_coins(acct2, acct1, amt);
+    }
+
+    #[ensures((old(self).balance(from, amt.denom) >= amt.amount) ==> result.is_ok())]
+    #[ensures(
+        result.is_ok() ==> forall(|acct_id: AccountId, coin_id: CoinId| acct_id != from && acct_id != to ==> self.balance(acct_id, coin_id) == old(self).balance(acct_id, coin_id))
+    )]
+    #[ensures(
+        result.is_ok() ==> forall(|coin_id: CoinId| coin_id != amt.denom ==> self.balance(from, coin_id) == old(self).balance(from, coin_id))
+    )]
+    #[ensures(
+        result.is_ok() ==> forall(|coin_id: CoinId| coin_id != amt.denom ==> self.balance(to, coin_id) == old(self).balance(to, coin_id))
+    )]
+    #[ensures(
+        result.is_ok() ==> forall(|coin_id: CoinId| coin_id == amt.denom ==> self.balance(to, coin_id) == old(self).balance(to, coin_id) + amt.amount)
+    )]
+    #[ensures(
+        result.is_ok() ==> forall(|coin_id: CoinId| coin_id == amt.denom ==> self.balance(from, coin_id) == old(self).balance(from, coin_id) - amt.amount)
+    )]
+    #[trusted]
+    fn send_coins(
+        &mut self,
+        from: AccountId,
+        to: AccountId,
+        amt: PrefixedCoin,
+    ) -> Result<(), Ics20Error>{
+        unimplemented!()
+    }
+
+    /// This function to enable minting ibc tokens to a user account
+    fn mint_coins(
+        &mut self,
+        account: AccountId,
+        amt: PrefixedCoin,
+    ) -> Result<(), Ics20Error> {
+        Ok(())
+    }
+
+    /// This function should enable burning of minted tokens in a user account
+    fn burn_coins(
+        &mut self,
+        account: AccountId,
+        amt: PrefixedCoin,
+    ) -> Result<(), Ics20Error> {
+        Ok(())
+    }
+}
+
+fn main(){}
