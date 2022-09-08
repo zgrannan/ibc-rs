d51ae3c4dc1dff7c99fbbfe0b8276de797739fac
Zack Grannan <zgrannan@protonmail.com>
2022-09-08 16:52:31 -0700

    WIP

--- token-swap/min.rs
+++ token-swap/min.rs
@@ -1,10 +1,16 @@
-#[derive(PartialEq)]
+
+fn path_eq(path: &Path, other: &Path) -> bool {
+        match (path, other) {
+            (Path::Cons(a1, b1), Path::Cons(a2, b2)) =>
+                *a1 == *a2 && *b1 == *b2,
+            _ => true,
+        }
+}
+
+// #[derive(PartialEq)]
 enum Path {
     Empty(),
-    Cons {
-        port: u32,
-        channel_end: u32
-    }
+    Cons(u32, u32)
 }
 
 fn main(){}
