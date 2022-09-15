c6aa090719cb3ca5758678f731745cc49df4ac0d
Zack Grannan <zgrannan@protonmail.com>
2022-09-15 12:21:02 -0700

    WIP

--- token-swap/swap.rs
+++ token-swap/swap.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code, unused)]
 use std::collections::HashMap;
+use std::convert::TryInto;
 use prusti_contracts::*;
 
 #[derive(Eq, PartialEq, Hash)]
@@ -11,7 +12,7 @@ struct ChannelEnd(u32);
 #[derive(Eq, PartialEq, Hash)]
 struct Port(u32);
 
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Eq, Hash)]
 enum Path {
     Empty(),
     Cons {
@@ -21,22 +22,57 @@ enum Path {
     }
 }
 
+// Prusti does not like derived PartialEQ
+impl PartialEq for Path {
+    #[trusted]
+    fn eq(&self, other: &Path) -> bool {
+        unimplemented!()
+    }
+
+}
+
 
 struct Bank {
     balances: HashMap<AccountID, HashMap<Coin,HashMap<Path, u32>>>
 }
 
-impl Bank {
+trait Bank {
 
-    fn update_balance(&mut self, acct_id: AccountID, path: Path, coin: Coin, amt: i32) {
+    #[pure]
+    #[trusted]
+    fn balance(&self, acct_id: &AccountID, path: &Path, coin: &Coin) -> u32 {
+        let acct_map = match self.balances.get(&acct_id) {
+            Some(m) => m,
+            None => return 0
+        };
+        let coin_map = match acct_map.get(&coin) {
+            Some(m) => m,
+            None => return 0
+        };
+        match coin_map.get(&path) {
+            Some(amt) => *amt,
+            None => 0
+        }
+    }
+
+    #[requires(amt >= -4294967295)]
+    #[requires(amt <= 4294967295)]
+    fn update_balance(&mut self, acct_id: AccountID, path: Path, coin: Coin, amt: i64) -> bool {
         let mut acct_map = self.balances.remove(&acct_id).unwrap_or(HashMap::new());
         let mut coin_map = acct_map.remove(&coin).unwrap_or(HashMap::new());
         let old_value = coin_map.remove(&path).unwrap_or(0);
-        coin_map.insert(path, ((old_value as i32) + amt) as u32);
+        let (success, new_value) = match ((old_value as i64) + amt).try_into() {
+           Ok(nv) => (true, nv),
+           Err(_) => (false, old_value)
+        };
+        coin_map.insert(path, new_value);
+        acct_map.insert(coin, coin_map);
+        self.balances.insert(acct_id, acct_map);
+        success
     }
 
-    fn mint_tokens(&mut self, to: AccountID, path: Path, coin: Coin, amt: i32) -> bool {
-        true
+    fn mint_tokens(&mut self, to: AccountID, path: Path, coin: Coin, amt: u32) -> bool {
+        self.update_balance(to, path, coin, amt as i64)
     }
 }
 
